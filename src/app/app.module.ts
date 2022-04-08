674fc259bc19ca622b98ec3356fb3b57651454e2
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-08 23:53:21 +0300

    imported ToDoModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from 'src/app/capp-routing.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
-import {ToDoModule } from 'src/app/to-do/to-do.module';
-import { ToDo } from './to-do/components/to-do/to-do.html/to-do.html.component'; 
+import {ToDoModule } from 'src/app/to-do/to-do/to-do.module';
+ 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToDo.HtmlComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-    ToDoModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule,ToDoModule],
   providers: [],
   bootstrap: [AppComponent]
 })
