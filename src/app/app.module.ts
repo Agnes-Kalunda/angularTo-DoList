27c8dfbfca4105334e29286b8007055798eceb0b
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-09 12:56:15 +0300

    edited file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import {ToDoModule } from 'src/app/to-do/to-do/to-do.module';
+import { ToDoComponent } from './to-do/components/to-do/to-do/to-do.component';
  
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ToDoComponent],
   imports: [BrowserModule, AppRoutingModule,ToDoModule],
   providers: [],
   bootstrap: [AppComponent]
