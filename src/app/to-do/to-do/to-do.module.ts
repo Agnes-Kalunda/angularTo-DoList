1aabad500b12ec7f29afa8dff88ad001363e21a7
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-09 13:38:58 +0300

    edited app.module

--- src/app/to-do/to-do/to-do.module.ts
+++ src/app/to-do/to-do/to-do.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToDoComponent } from '../components/to-do/to-do/to-do.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ToDoComponent ],
   imports: [
     CommonModule
   ]
